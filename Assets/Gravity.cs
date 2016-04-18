910dd7a83a28e7f0f11a4c2dfbbf6dcbcbc77ed7
Aljaz <aljaz.blazej@gmail.com>
2016-04-18 18:49:34 +0200

    First prototype

---
+++ Assets/Gravity.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+
+	public GameObject rotationReference;
+
+	void Start() {
+		Cursor.visible = false;
+	}
+
+	void Update () {
+		if (Input.GetKeyDown("g")) {
+			Physics.gravity = -Physics.gravity;
+			rotationReference.SendMessage("changeGravity");
+		}
+	}
+}
---
+++ Assets/ReverseRotation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReverseRotation : MonoBehaviour {
+	
+	private bool rotate = false;
+	private int gravityDir;
+	private float rotSpeed = 1000f;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		gravityDir = (int)(-Physics.gravity.y / 9.81f);
+
+		if (rotate) {
+			if (gravityDir == 1) {
+				if (transform.eulerAngles.z > 0 && transform.eulerAngles.z < 180) {
+					transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0); 
+					rotate = false;
+				} else {
+					transform.eulerAngles += Vector3.forward * rotSpeed * Time.deltaTime;
+				}
+			} else if (gravityDir == -1) {
+				if (transform.eulerAngles.z < 180 && transform.eulerAngles.z > 0) {
+					transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 180); 
+					rotate = false;
+				} else {
+					transform.eulerAngles += Vector3.back * rotSpeed * Time.deltaTime;
+				}
+			}
+		}
+	}
+
+	private void changeGravity() {
+		rotate = true;
+	}
+}
